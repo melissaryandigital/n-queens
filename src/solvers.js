72a5f694aa54297728beae19370813060d45f7a2
Melissa Ryan <melissaryandigital@gmail.com>
2020-11-14 18:56:53 -0600

    Completed findNQueensSolution

--- src/solvers.js
+++ src/solvers.js
@@ -27,11 +27,8 @@ window.findNRooksSolution = function (n) {
       togglePiece(i, i);
     }
   }
-  // Iterate through the attributes of newBoard (as long as it's not 'n')
-  // solution.push(row);
-  for (var key in newBoard.rows()) {
-    solution.push(newBoard.get(key));
-  }
+
+  solution = newBoard.rows();
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -42,10 +39,10 @@ window.countNRooksSolutions = function (n) {
   var solutionCount = 0;
   var newBoard = new Board({ 'n': n });
 
-  var solutionBoard = function (n, currentRow) {
+  var solutionBoard = function (n, currentRowIndex) {
+
     // Base case
-    // If rowIndex === n
-    if (currentRow === n) {
+    if (currentRowIndex === n) {
       // Increment solutionCount
       solutionCount++;
       return;
@@ -58,15 +55,15 @@ window.countNRooksSolutions = function (n) {
     for (var i = 0; i < n; i++) {
 
       // Put the piece down
-      newBoard.togglePiece(currentRow, i);
+      newBoard.togglePiece(currentRowIndex, i);
 
       // Call hasAnyRowConflicts
       if (newBoard.hasAnyRooksConflicts()) {
         // If there is a conflict, toggle the piece off (pick it up)
-        newBoard.togglePiece(currentRow, i);
+        newBoard.togglePiece(currentRowIndex, i);
       } else {
-        solutionBoard(n, currentRow + 1);
-        newBoard.togglePiece(currentRow, i);
+        solutionBoard(n, currentRowIndex + 1);
+        newBoard.togglePiece(currentRowIndex, i);
       }
 
     }
@@ -89,10 +86,43 @@ window.countNRooksSolutions = function (n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function (n) {
-  var solution = undefined; //fixme
+  var newBoard = new Board({ 'n': n });
+  var solution = [];
+
+  var solutionBoard = function (n, currentRowIndex) {
+
+    //base case: when the passed in currentRowIndex equals n (i.e. the row doesn't exist) then we know we've reached the end of the process and found a board that is valid
+    //set the solution equal to the valid board
+    //return solution.
+
+    if (currentRowIndex === n) {
+      if (solution.length !== n) {
+        for (var t = 0; t < newBoard.rows().length; t++) {
+          solution.push(newBoard.get(t).slice());
+        }
+      }
+
+      return;
+    }
+
+    for (var i = 0; i < n; i++) {
+      newBoard.togglePiece(currentRowIndex, i);
+
+      if (!newBoard.hasAnyQueensConflicts()) {
+        solutionBoard(n, currentRowIndex + 1);
+      }
+      newBoard.togglePiece(currentRowIndex, i);
+    }
+  };
+
+  solutionBoard(n, 0);
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  if (solution.length === 0) {
+    return newBoard.rows();
+  } else {
+    return solution;
+  }
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
